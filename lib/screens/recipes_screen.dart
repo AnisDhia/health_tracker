6a97f9eae1ea1c6b44c9c26f8970054ee974b5b3
anis <aanisgamer@gmail.com>
2022-03-19 21:16:49 +0100

    Updated some packages updated  recipes screen

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:health_tracker/model/recipe.api.dart';
-import 'package:health_tracker/model/recipe.dart';
 import 'package:health_tracker/widgets/drawer.dart';
-import 'package:health_tracker/widgets/recipe_card.dart';
 
 class RecipesScreen extends StatefulWidget {
   const RecipesScreen({ Key? key }) : super(key: key);
@@ -12,23 +9,6 @@ class RecipesScreen extends StatefulWidget {
 }
 
 class _RecipesScreenState extends State<RecipesScreen> {
-
-  late List<Recipe> _recipes;
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    getRecipes();
-  }
-
-  Future<void> getRecipes() async{
-    _recipes = await RecipeApi.getRecipe();
-    setState(() {
-      _isLoading = false;
-    });
-  }
   
   @override
   Widget build(BuildContext context) {
@@ -38,17 +18,7 @@ class _RecipesScreenState extends State<RecipesScreen> {
         title: const Text('Recipes'), 
         centerTitle: true,
       ),
-      body: _isLoading ? const Center(child: CircularProgressIndicator()) : ListView.builder(
-        itemCount: _recipes.length,
-        itemBuilder: (context, index){
-          return RecipeCard(
-            title: _recipes[index].name, 
-            cookTime: _recipes[index].totalTime, 
-            rating: _recipes[index].rating.toString(), 
-            thumbnailUrl: _recipes[index].images
-            );
-        },
-      )
+      body: const Text("Recipes") 
     );
   }
 }
