888b6d960673fa18ae491e084c2d8d5924281ee8
anis <aanisgamer@gmail.com>
2022-05-27 14:50:25 +0100

    Updated Food Details Screen

--- lib/ui/screens/diary/nutrition/food_details_screen.dart
+++ lib/ui/screens/diary/nutrition/food_details_screen.dart
@@ -1,7 +1,10 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:health_tracker/data/models/food_model.dart';
 import 'package:health_tracker/data/repositories/firestore.dart';
 import 'package:health_tracker/shared/services/firestore_service.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:pie_chart/pie_chart.dart' as pie;
 
 class FoodDetailsScreen extends StatefulWidget {
   const FoodDetailsScreen({Key? key, required this.food, required this.meal})
@@ -15,12 +18,41 @@ class FoodDetailsScreen extends StatefulWidget {
 
 class _FoodDetailsScreenState extends State<FoodDetailsScreen> {
   late String meal;
+  TimeOfDay time = TimeOfDay.now();
+  late TextEditingController servingsController;
+  late TextEditingController servingUnitController;
+  late Map<String, dynamic> serving;
+  late Map<String, dynamic> protein, fat, carbs, calories;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    servingUnitController = TextEditingController();
+    servingsController = TextEditingController();
     meal = widget.meal;
+    serving = {'size': 100, 'unit': 'g'};
+    protein = widget.food.nutrientFromMap(1003);
+    fat = widget.food.nutrientFromMap(1004);
+    carbs = widget.food.nutrientFromMap(1005);
+    calories = widget.food.nutrientFromMap(1008);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    servingUnitController.dispose();
+    servingsController.dispose();
+    super.dispose();
+  }
+
+  double macroPercentage(double value, int type) {
+    double calories = protein['value'] * 4 + carbs['value'] * 4 + fat['value'] * 9;
+    if (type == 1) {
+      return (value * 4 / calories) * 100;
+    } else {
+      return (value * 9 / calories) * 100;
+    }
   }
 
   @override
@@ -108,9 +140,9 @@ class _FoodDetailsScreenState extends State<FoodDetailsScreen> {
                 const Expanded(child: Text('Number of Servings')),
                 TextButton(
                     onPressed: () {
-                      // showDialog(context: context, builder: builder)
+                      _servingsDialog(context);
                     },
-                    child: const Text('will be added soon'))
+                    child: Text(widget.food.numberOfServings.toString()))
               ],
             ),
             const Divider(),
@@ -119,12 +151,112 @@ class _FoodDetailsScreenState extends State<FoodDetailsScreen> {
                 const Expanded(child: Text('Serving Size')),
                 TextButton(
                     onPressed: () {
-                      // showDialog(context: context, builder: builder)
+                      _servingsDialog(context);
                     },
-                    child: const Text('will be added soon'))
+                    child: Text(
+                        '${widget.food.servingSize} ${widget.food.servingSizeUnit}'))
               ],
             ),
             const Divider(),
+            Row(
+              children: [
+                const Expanded(child: Text('Time')),
+                TextButton(
+                  onPressed: () async {
+                    final newTime = await showTimePicker(
+                        context: context, initialTime: TimeOfDay.now());
+
+                    if (newTime == null) return;
+
+                    setState(() {
+                      time = newTime;
+                    });
+                  },
+                  child: Text('${time.hour}:${time.minute}',
+                      style: const TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(calories['value'].toString()),
+                        Text(calories['unitName'].toLowerCase()),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 90,
+                      width: 90,
+                      child: pie.PieChart(
+                        dataMap: {
+                          'Carbs': carbs['value'],
+                          'Fat': fat['value'],
+                          'Protein': protein['value'],
+                        },
+                        chartType: pie.ChartType.ring,
+                        baseChartColor: Colors.grey.shade900,
+                        colorList: const [
+                          Color.fromARGB(255, 0, 210, 124),
+                          Color.fromARGB(255, 128, 71, 246),
+                          Color.fromARGB(255, 254, 164, 44)
+                        ],
+                        legendOptions:
+                            const pie.LegendOptions(showLegends: false),
+                        chartValuesOptions: const pie.ChartValuesOptions(
+                            showChartValues: false),
+                        ringStrokeWidth: 6,
+                        
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '${macroPercentage(carbs['value'], 1).ceil()}%',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 210, 124)),
+                    ),
+                    Text('${carbs['value']} ${carbs['unitName'].toLowerCase()}',
+                        style: const TextStyle(fontSize: 20)),
+                    const Text('Carbs'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('${macroPercentage(fat['value'], 2).floor()}%',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 128, 71, 246))),
+                    Text('${fat['value']} ${fat['unitName'].toLowerCase()}',
+                        style: const TextStyle(fontSize: 20)),
+                    const Text('Fat'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('${macroPercentage(protein['value'], 1).ceil()}%',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 254, 164, 44))),
+                    Text(
+                        '${protein['value']} ${protein['unitName'].toLowerCase()}',
+                        style: const TextStyle(fontSize: 20)),
+                    const Text('Protein'),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
             Expanded(
                 child: ListView.builder(
                     itemCount: widget.food.nutrients.length,
@@ -149,5 +281,72 @@ class _FoodDetailsScreenState extends State<FoodDetailsScreen> {
         ),
       ),
     );
+  }
+
+  Future<dynamic> _servingsDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            contentPadding: const EdgeInsets.all(16),
+            title: const Text('How Much?'),
+            children: [
+              const Text('How Much?'),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: servingsController,
+                    ),
+                  ),
+                  const Text('Serving(s) of')
+                ],
+              ),
+              DropdownButton<Map<String, dynamic>>(
+                  value: serving,
+                  items: [
+                    DropdownMenuItem(
+                      value: serving,
+                      child: Text('${serving['size']} ${serving['unit']}'),
+                    ),
+                    DropdownMenuItem(
+                      value: {
+                        'size': widget.food.servingSize,
+                        'unit': widget.food.servingSizeUnit
+                      },
+                      child: Text(
+                          '${widget.food.servingSize} ${widget.food.servingSizeUnit}'),
+                    ),
+                  ],
+                  onChanged: (newValue) {
+                    setState(() {
+                      serving = newValue!;
+                    });
+                  }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        serving['size'] = servingsController.text.trim();
+                        serving['unit'] = servingUnitController.text.trim();
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: const Text('save'),
+                  )
+                ],
+              )
+            ],
+          );
+        });
   }
 }
