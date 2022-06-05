ce032e30def012ee33aeeaa80eef8672f8df18a3
anis <aanisgamer@gmail.com>
2022-06-05 02:17:45 +0100

    Updated Recipes Screen added categories

--- lib/ui/screens/recipes/recipes_screen.dart
+++ lib/ui/screens/recipes/recipes_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:health_tracker/data/models/recipe_model.dart';
 import 'package:health_tracker/data/repositories/spoonacular_api.dart';
+import 'package:health_tracker/ui/screens/recipes/recipe_details_screen.dart';
 import 'package:health_tracker/ui/screens/recipes/widgets/recipe_card_widget.dart';
 import 'package:health_tracker/ui/widgets/indicator_widget.dart';
 import 'package:tab_indicator_styler/tab_indicator_styler.dart';
@@ -43,14 +44,14 @@ class _RecipesScreenState extends State<RecipesScreen> {
                   isScrollable: true,
                   tabs: [
                     Tab(
-                      text: 'New Recipes'.toUpperCase(),
+                      text: 'Categories'.toUpperCase(),
                     ),
                     Tab(
                       text: 'Favourites'.toUpperCase(),
                     ),
                     Tab(
-                      text: 'Categories'.toUpperCase(),
-                    )
+                      text: 'New Recipes'.toUpperCase(),
+                    ),
                   ],
                   labelColor:
                       Theme.of(context).tabBarTheme.labelColor, //Colors.black,
@@ -66,12 +67,12 @@ class _RecipesScreenState extends State<RecipesScreen> {
                 ),
                 Expanded(
                   child: TabBarView(children: [
+                    const RecipeCategories(),
+                    const Center(child: Text("Favourites")),
                     NewRecipe(
                       newRecipesList: recipeList,
                     ),
-                    const Center(child: Text("New")),
                     // NewRecipe(),
-                    const RecipeCategories(),
                   ]),
                 )
               ],
@@ -96,79 +97,121 @@ class _RecipeCategoriesState extends State<RecipeCategories> {
           child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          children: [
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('Keto', style: TextStyle(fontSize: 24),),
             SizedBox(
-              height: 230,
-              child: FutureBuilder(
-                  future: SpoonacularService.instance
-                      .getRandomRecipes(number: 15, tags: "keto"),
-                  builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
-                    if (!snapshot.hasData) {
-                      return const MyCircularIndicator();
-                    } else {
-                      // List<Recipe> recipes = snapshot.data!;
-                      return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        physics: const ScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          bool bookmarked = false;
-                          return Card(
-                            clipBehavior: Clip.antiAlias,
-                            elevation: 10,
-                            // margin: const EdgeInsets.all(8.0),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                            child: SizedBox(
-                              width: 150,
-                              // height: 250,
-                              child: Column(children: [
-                                Image(
-                                    // width: 150,
-                                    // height: 100,
-                                    image: NetworkImage(
-                                        snapshot.data![index].imageUrl)),
-                                const SizedBox(
-                                  height: 12,
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: Text(
-                                        snapshot.data![index].title,
-                                        // softWrap: false,
-                                        // overflow: TextOverflow.fade,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 12,
-                                ),
-                                Row(children: [
-                                  IconButton(
-                                      onPressed: () {
-                                        bookmarked = !bookmarked;
-                                        // setState(() {
-                                          
-                                        // });
-                                      },
-                                      icon: Icon(bookmarked
-                                          ? Icons.bookmark
-                                          : Icons.bookmark_border))
-                                ])
-                              ]),
-                            ),
-                          );
-                        },
-                      );
-                    }
-                  }),
-            )
+              height: 4,
+            ),
+            HorizontalRecipes(category: "keto"),
+            SizedBox(
+              height: 8,
+            ),
+            Text('Vegetarian', style: TextStyle(fontSize: 24),),
+            SizedBox(
+              height: 4,
+            ),
+            HorizontalRecipes(category: "vegetarian"),
+            SizedBox(
+              height: 8,
+            ),
+            Text('Pescetarian', style: TextStyle(fontSize: 24),),
+            SizedBox(
+              height: 4,
+            ),
+            HorizontalRecipes(category: "pescetarian"),
           ],
         ),
       )),
+    );
+  }
+}
+
+class HorizontalRecipes extends StatelessWidget {
+  const HorizontalRecipes({Key? key, required this.category}) : super(key: key);
+  final String category;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      child: FutureBuilder(
+          future: SpoonacularService.instance
+              .getRandomRecipes(number: 15, tags: category),
+          builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
+            if (!snapshot.hasData) {
+              return const MyCircularIndicator();
+            } else {
+              // List<Recipe> recipes = snapshot.data!;
+              return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                physics: const ScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  bool bookmarked = false;
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    elevation: 10,
+                    // margin: const EdgeInsets.all(8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RecipeDetails(
+                                recipeModel: snapshot.data![index],
+                              ),
+                            ));
+                      },
+                      child: SizedBox(
+                        width: 150,
+                        // height: 250,
+                        child: Column(children: [
+                          Image(
+                              // width: 150,
+                              // height: 100,
+                              image:
+                                  NetworkImage(snapshot.data![index].imageUrl)),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  snapshot.data![index].title,
+                                  // softWrap: false,
+                                  // overflow: TextOverflow.fade,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Row(children: [
+                            IconButton(
+                                onPressed: () {
+                                  bookmarked = !bookmarked;
+                                  // setState(() {
+
+                                  // });
+                                },
+                                icon: Icon(bookmarked
+                                    ? Icons.bookmark
+                                    : Icons.bookmark_border))
+                          ])
+                        ]),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          }),
     );
   }
 }
